3d271b3ffeb2824cb0b483b7b6ddef02ef6915d4
Roger Rosset <rrosset91@gmail.com>
2026-02-10 12:10:23 +0000

    Add verbose logging to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			onwarn(warning, warn) {
+				// Log all warnings
+				console.warn('Rollup warning:', warning.message);
+				warn(warning);
+			}
+		}
+	}
 });
